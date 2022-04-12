cb7354a74148fef6504b9f1824d9762b42dbdf07
Bulbash <zverovik@yahoo.com>
2022-04-12 10:17:47 +0300

    delete method and import

--- src/main/java/com/alex/mappers/HumanMapper.java
+++ src/main/java/com/alex/mappers/HumanMapper.java
@@ -3,9 +3,6 @@ package com.alex.mappers;
 import com.alex.dto.HumanDto;
 import com.alex.model.Human;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.factory.Mappers;
-import org.springframework.stereotype.Component;
 
 @Mapper
 public interface HumanMapper {
