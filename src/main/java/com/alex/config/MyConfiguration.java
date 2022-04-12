2b52f56b787495617f71c860eeea5fb0b80fad41
Bulbash <zverovik@yahoo.com>
2022-04-12 10:16:47 +0300

    added create beans

--- src/main/java/com/alex/config/MyConfiguration.java
+++ src/main/java/com/alex/config/MyConfiguration.java
@@ -1,9 +1,13 @@
 package com.alex.config;
 
-import com.alex.dto.HumanDto;
-import com.alex.dto.PhoneDto;
-import com.alex.model.Human;
-import com.alex.model.Phone;
+import com.alex.controllers.HumanController;
+import com.alex.controllers.PhoneController;
+import com.alex.mappers.HumanMapper;
+import com.alex.mappers.HumanMapperImpl;
+import com.alex.mappers.PhoneMapper;
+import com.alex.mappers.PhoneMapperImpl;
+import com.alex.service.HumanService;
+import com.alex.service.PhoneService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,30 +17,30 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class MyConfiguration {
 
     //todo читать про способы внедрения зависимостей
-//    @Bean
-//    public HumanMapper humanMapperBean(){
-//        return new HumanMapper();
-//    }
-//    @Bean
-//    public PhoneMapper phoneMapperBean(){
-//        return new PhoneMapper();
-//    }
-//    @Bean
-//    public HumanService humanServiceBean(HumanDaoImpl humanDao, HumanMapper humanMapper){
-//        return new HumanService(humanDao,humanMapper);
-//    }
+    @Bean
+    public HumanMapper humanMapperBean(){
+        return new HumanMapperImpl();
+    }
+    @Bean
+    public PhoneMapper phoneMapperBean(){
+        return new PhoneMapperImpl();
+    }
+    @Bean
+    public HumanService humanServiceBean(){
+        return new HumanService();
+    }
 
-//    @Bean
-//    public PhoneService phoneServiceBean(PhoneDaoImpl phoneDao , PhoneMapper phoneMapper){
-//        return new PhoneService(phoneDao,phoneMapper);
-//    }
-//    @Bean
-//    public HumanController humanControllerBean(HumanService humanService){
-//        return new HumanController(humanService);
-//    }
+    @Bean
+    public PhoneService phoneServiceBean(){
+        return new PhoneService();
+    }
+    @Bean
+    public HumanController humanControllerBean(){
+        return new HumanController();
+    }
 
-//    @Bean
-//    public PhoneController phoneControllerBean(PhoneService phoneService){
-//        return new PhoneController(phoneService);
-//    }
+    @Bean
+    public PhoneController phoneControllerBean(){
+        return new PhoneController();
+    }
 }
